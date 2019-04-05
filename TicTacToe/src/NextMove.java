9a08bb1d9683c10cedd42d57927b147fa5926158
akshatgupta342 <42548487+akshatgupta342@users.noreply.github.com>
2019-04-05 10:47:51 +0530

    Update NextMove.java

--- TicTacToe/src/NextMove.java
+++ TicTacToe/src/NextMove.java
@@ -1,4 +1,4 @@
-/*
+/* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -6,7 +6,7 @@
 
 /**
  *
- * @author root
+ * @author GrayHat and AkshatGupta
  */
 //0 computer
 //x human
